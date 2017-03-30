94da15b6f6e5851e326e9336ab575e780981fdb8
dshelters <dshelters@gmail.com>
2017-03-30 16:09:42 -0700

    (feat) Create Nav component

---
+++ react-client/src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class Nav extends React.Component {
+  constructor({selections}) {
+    super({selections});
+    this.state = {};
+  }
+
+  render () {
+    return (
+      
+    )
+  }
+}
+
+export default Nav;
