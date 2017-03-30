2124454f77f5d0a8ce8cc74b6802bd23ebecec29
Jonathan Chou <jonchou@ucdavis.edu>
2017-03-30 16:34:31 -0700

    (refactor) bring back centering of search bar.

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -256,7 +256,11 @@ class App extends React.Component {
             handleUserWantsLogin={this.handleUserWantsLogin} 
             userHasSearched={this.state.userHasSearched}
           />
-          <Search search={this.search} />
+          <div className = 'heroImageContainer'>
+            <div className = 'heroContentWrapper'>
+              <Search className ='SearchBar' search={this.search} />
+            </div>
+          </div>
         </div>
         <Router>
           <div>        
@@ -266,10 +270,10 @@ class App extends React.Component {
               <li><Link to='/dummy'>Dummy</Link></li>
             </ul>
             <hr/>
-              <Route exact path='/' component={Homepage}/>
-              <Route path='/products' component={Products}/>
-              <Route path='/product/overview' component={ProductOverviewComp}/>
-              <Route path='/dummy' component={Dummy} />
+            <Route exact path='/' component={Homepage}/>
+            <Route path='/products' component={Products}/>
+            <Route path='/product/overview' component={ProductOverviewComp}/>
+            <Route path='/dummy' component={Dummy} />
           </div>
         </Router>
       </div>
