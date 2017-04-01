4365dac99f372c947c824803d302e0e38d349d82
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-01 11:48:08 -0700

    Finish button and render survey onClick

--- react-client/src/components/TopBar.jsx
+++ react-client/src/components/TopBar.jsx
@@ -14,18 +14,18 @@ class TopBar extends React.Component {
   render(){
     var signup = (<button className='flexItem flexEdge' onClick={this.props.handleUserWantsSignUp} value='signup'>Sign Up</button>);
     var home = (<button onClick={() => {this.props.handleClickedNavItem(-1)}} className='flexItem flexEdge' value='login'>Home</button>);
-    var questionnaire = (<button className='flexItem flexEdge' value='login'>Questionnaire</button>);
+    var questionnaire = (<button className='flexItem flexEdge'>Survey</button>);
 
     return(
       <div>
         <Link to='/'>
           <div className='flexContainer'>
-            { this.props.userWantsHomePage && !this.props.userLoggedIn ? signup : this.props.userWantsHomePage && this.props.userLoggedIn || this.props.userWantsLogin ? home : home}
+            {this.props.userWantsHomePage && !this.props.userLoggedIn ? signup : this.props.userWantsHomePage && this.props.userLoggedIn || this.props.userWantsLogin ? home : home}
           </div>  
         </Link>
         <Link to='/questionnaire'>
           <div className='flexContainer'>
-            { questionnaire }
+            {questionnaire}
           </div>   
         </Link>
       </div>
