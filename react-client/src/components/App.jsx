3ec125b8d203e2c310ce326df464ea7befc4a0db
Benicio Peinado <BenicioPeinado@gmail.com>
2017-03-31 21:14:46 -0700

    Prepare code for PR

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -284,7 +284,6 @@ class App extends React.Component {
                   userHasSearched={this.state.userHasSearched}
                 />
               </Link>
-              <Questionnaire />
               <div className = 'heroImageContainer'>
                 <div className = 'heroContentWrapper'>
                   Uva 2.Grape
